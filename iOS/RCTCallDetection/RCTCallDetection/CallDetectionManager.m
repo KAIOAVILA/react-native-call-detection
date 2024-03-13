c372ebefdc463f787c1a95c9bff7693f6e6c8c1c
max.wang <mstar0223@gmail.com>
2024-03-13 16:40:37 +0800

    Fixed events

--- iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
+++ iOS/RCTCallDetection/RCTCallDetection/CallDetectionManager.m
@@ -7,7 +7,7 @@
 //  Copyright © 2017 Facebook. All rights reserved.
 //
 
-##import "CallDetectionManager.h"
+#import "CallDetectionManager.h"
 @import CallKit;
 
 typedef void (^CallBack)();
@@ -68,15 +68,12 @@ RCT_EXPORT_METHOD(stopListener) {
 - (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
     if (call.hasEnded == true) {
       [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Disconnected"];
-    }
-    if (call.isOutgoing == true && call.hasConnected == false && call.hasEnded == false) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
-    }
-    if (call.isOutgoing == false && call.hasConnected == false) {
-      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
-    }
-    if (call.hasEnded == false && call.hasConnected == true) {
+    } else if (call.hasConnected == true) {
       [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Connected"];
+    } else if (call.isOutgoing == true) {
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Dialing"];
+    } else {
+      [self sendEventWithName:@"PhoneCallStateUpdate" body:@"Incoming"];
     }
 }
 
